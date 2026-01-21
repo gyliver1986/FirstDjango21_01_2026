759d33c99646de03b7bcf0a2f5183762f97f379f
Alexander Gulin <gonimani1@gmail.com>
2026-01-21 20:38:48 +0300

    one push

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,2 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse(f'<h1>"Изучаем django"</h1><strong>Автор</strong>: <i>Иванов И.П.</i>' )
+                        
+                        
 
